3bf5e464f4d49b5d3916d76340bd60e594d8bfa9
taoli24 <gujie713@gmail.com>
2022-07-14 15:31:34 +1000

    added comments to main.py

--- main.py
+++ main.py
@@ -77,11 +77,16 @@ def get_quiz_list(number_of_questions: int, user_choice_of_category: int) -> lis
 
 
 if __name__ == '__main__':
+    # retrieve categories from public api
     categories = get_categories()
+    # display categories to user
     display_category(categories)
+    # verify and get user input
     number_questions, user_choice = verify_user_choice(categories)
+    # initialise quiz controller object
     quiz_controller = QuizControl(get_quiz_list(number_questions, user_choice))
 
+    # main logic - run when there is still questions
     while quiz_controller.still_has_questions():
         user_answer = None
         question_text = quiz_controller.next_question()
@@ -90,9 +95,11 @@ if __name__ == '__main__':
             if user_answer not in quiz_controller.current_question.acceptable_answers:
                 print(f'Please answer with {quiz_controller.current_question.acceptable_answers}')
         if quiz_controller.check_answer(user_answer):
-            print(f"{Back.GREEN}That was correct.{Style.RESET_ALL}")
+            print(f"{Back.GREEN}That was correct.{Style.RESET_ALL}\n")
         else:
-            print(f"{Back.RED}Sorry, that was wrong.{Style.RESET_ALL}")
+            print(f"{Back.RED}Sorry, that was wrong.{Style.RESET_ALL}\nThe correct answer is {quiz_controller.current_question.answer}.\n")
 
+
+    # report score
     print(
         f'Your score is {quiz_controller.score}/{number_questions} with an accuracy of {quiz_controller.score / number_questions * 100:.2f}%')
