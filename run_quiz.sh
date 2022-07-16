2afafed2b37929e657863c51032898645b2c168b
taoli24 <gujie713@gmail.com>
2022-07-17 09:59:57 +1000

    updated bash script

--- run_quiz.sh
+++ run_quiz.sh
@@ -7,9 +7,14 @@ if [ "$1" == "--log" ] && [ $# -eq 1 ]; then
     echo "Log file does not exist! Play a game first!"
   fi
 
-else
+elif [ $# -eq 1 ] && [ "$1" == "--help" ]; then
+  cat help.txt
+
+elif [ $# -eq 0 ]; then
   clear
   python3 main.py
+else
+  echo "Unknown parameter, try run ./run_quiz.sh --help"
 fi
 
 
